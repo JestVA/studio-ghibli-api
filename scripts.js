1d27a735fc65b3bedf72097e23c90e59793a29cf
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-27 13:14:39 +0000

    create element for logo and add logo to the project

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 const app = document.getElementById('root');
+const logo = document.createElement('img');
+logo.src = 'logo.png';
 
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest();
