cf1117a62e2eb92f5a8e6401d34a1a905e2b377d
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-27 13:24:58 +0000

    create card component together with p and h1 elements to render data from the json response and use substring() method to limit the text length

--- scripts.js
+++ scripts.js
@@ -19,7 +19,16 @@ request.onload = function () {
   if (request.status >= 200 && request.status < 400) {
     data.forEach(movie => {
       // Log each movie's title
-      console.log(movie.title);
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+      const h1 = document.createElement('h1');
+      h1.textContent = movie.title;
+      const p = document.createElement('p');
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`;
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
     });
   } else {
     console.log('error');
